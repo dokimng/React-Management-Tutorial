42e3a17aad76917e87a2ebd44a005e7dcc7764fe
dokimng <kdhyun010619@gmail.com>
2022-07-16 20:18:32 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/CustomerId'
 import './App.css';
 
-class App extends Component{
-  render(){
-    return(
-      <div className='gray-background'>
-        <img src={logo} lat="logo" alt='실패'></img>
-        <h2>Let's develop management system!</h2>
-      </div>
-    )
-  }
+const customer = [{
+  'id' : 1,
+  'image' : 'http://placeimg.com/64/64/1',
+  'name' : '김동현',
+  'birthday' : '010619',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'http://placeimg.com/64/64/2',
+  'name' : 'ddd',
+  'birthday' : '020619',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 3,
+  'image' : 'http://placeimg.com/64/64/3',
+  'name' : 'aaa',
+  'birthday' : '010619',
+  'gender' : '남자',
+  'job' : '대학생'
 }
+]
+
+class App extends Component{
+  
+  render(){
+    return (
+      customer.map(c => {
+        return(
+          <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}></Customer>
+        )
+      })
+    );
+  } 
+}
+
+
 export default App;
